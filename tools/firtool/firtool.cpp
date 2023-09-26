9f71eb0b709fbf3306b6cdc8c0e7e2c6925f4240
Will Dietz <will.dietz@sifive.com>
2023-09-26 08:59:43 -0500

    Revert "firtool: llvm_shutdown before exit() for bringing down thread pool."
    Test failure encountered.
    
    This reverts commit e531fb400fe075c12aae05de3f68965592d7cd5c.

--- tools/firtool/firtool.cpp
+++ tools/firtool/firtool.cpp
@@ -650,12 +650,5 @@ int main(int argc, char **argv) {
   // Do the guts of the firtool process.
   auto result = executeFirtool(context);
 
-  // Ensures thread pool is stopped to avoid intermittent crashes on Windows.
-  // https://reviews.llvm.org/D70447 for some related discussion.
-  llvm_shutdown();
-
-  // Use "exit" instead of return'ing to signal completion.  This avoids
-  // invoking the MLIRContext destructor, which spends a bunch of time
-  // deallocating memory etc which process exit will do for us.
-  exit(failed(result));
+  return succeeded(result) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
